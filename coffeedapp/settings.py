04cf7b72d19a282048f3692fc00031879b06c491
papajo <joshipv2@gmail.com>
2015-04-08 20:38:31 -0400

    Commit #2 for a review on the boostrap issue

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
     }
 }
 
