726baeac8248ebb3df200a15cb69b4d666953422
papajo <joshipv2@gmail.com>
2015-04-10 14:57:22 -0400

    Absolute Location URL part needs a review

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+class Location(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+    	return self.title
+
+   	def get_absolute_url(self):
+   		return reverse(viewname="location_list", args=[self.id])
